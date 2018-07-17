8c508cf651fe426bd96ffb2b8d9837dbd60c4b08
LindaKadz <achielinda44@gmail.com>
2018-07-17 12:16:37 +0300

    Testing the db columns

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "db structure" do
+  it { is_expected.to have_db_column(:balance)  }
+  it { is_expected.to have_db_column(:pin)  }
+  it { is_expected.to have_db_column(:account_number)  }
+  it { is_expected.to have_db_column(:id)  }
+end 
 end
