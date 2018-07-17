fc58fce9badfb97373f3022a4ee3f4dd7350e990
LindaKadz <achielinda44@gmail.com>
2018-07-17 15:05:51 +0300

    testing validations for balance, pin and account_number

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,8 @@
 class Account < ApplicationRecord
+  validates :balance, numericality: {only_integer: true}
+  validates :pin, numericality: {only_integer: true}
+  validates :balance, numericality: {greater_than: 0}
+  validates :pin, length: { is: 4 }
+  validates :account_number, numericality: {only_integer: true}
+  validates :account_number, uniqueness: true
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -11,4 +11,21 @@ end
  it "has valid factories" do
    expect(FactoryBot.build(:account)).to be_valid
  end
+
+ describe "Balance Validation" do
+   context "Account balance" do
+   it { is_expected.to validate_numericality_of(:balance).only_integer}
+   it { is_expected.to validate_numericality_of(:balance).is_greater_than(0)}
+   end
+
+  context "Account Pin" do
+   it { is_expected.to validate_numericality_of(:pin).only_integer}
+   
+  end
+
+  context "Account Number" do
+    it { is_expected.to validate_numericality_of(:account_number).only_integer}
+    it { is_expected.to validate_uniqueness_of(:account_number)}
+  end
+ end
 end
