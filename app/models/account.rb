6d35e063fb41e6efe9f78ccacc99963eb9d4d93b
LindaKadz <achielinda44@gmail.com>
2018-07-18 09:55:44 +0300

    Validating pin

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,7 @@ class Account < ApplicationRecord
   validates :balance, numericality: {only_integer: true}
   validates :pin, numericality: {only_integer: true}
   validates :balance, numericality: {greater_than: 0}
+  validates :balance, numericality: {less_than: 3000}
   validates :pin, length: { is: 4 }
   validates :account_number, numericality: {only_integer: true}
   validates :account_number, uniqueness: true
